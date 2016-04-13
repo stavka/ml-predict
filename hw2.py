2ded7bba29d91c89389455c7c396e1495a7a4bca
stavka <stavka@gmail.com>
2016-04-13 09:24:33 -0400

    all 4 with simple parameter search

--- hw2.py
+++ hw2.py
@@ -15,7 +15,7 @@ from keras.regularizers import l1,l2,l1l2
 from keras.callbacks import EarlyStopping
 from time import time
 import numpy as np
-import pprint
+from pprint import pprint
 import scipy.optimize as scipy_opt
 from math import isnan
 
@@ -38,109 +38,52 @@ def timed_sgd_OLS(x1,y1,x2=None,y2=None,
                   lr=0.1, decay=1e-2, nesterov=True, momentum=0.8, batch_size=100,nb_epoch=50):
     if (x2 is None)^(x2 is None): raise ValueError("if you specify x2 or y2 you need to specify the other as well")
     if x2 is None and y2 is None: x2=x1; y2=y1 #if no Cross-validation set, use original
+    print("Running SGD OLS with lr: ", lr, " decay: ", decay, " Momentum: ", momentum, " Batch size: ", batch_size)
+    np.random.seed(78)
     time0 = time() # Start timer
-    earlystopper = EarlyStopping(monitor='loss', patience=1, verbose=1)
+    earlystopper = EarlyStopping(monitor='loss', patience=1)
     sgd=SGD(lr=lr, decay=decay, nesterov=nesterov, momentum=momentum)
     model = Sequential()
     model.add(Dense(1, input_dim=K, activation='linear'))
     model.compile(loss='mse', optimizer=sgd)
-    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True, callbacks=[earlystopper])
+    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True, callbacks=[earlystopper], verbose=0)
     score = model.evaluate(x2, y2, batch_size=20)
     #print("Score:",score,'\n',np.around(model.layers[0].get_weights()[0][:,0],2))
     #print("Time:",time()-time0)
     return time()-time0, score
-
-
-
-results = []
-
-for bs in (10, 50, 100, 200, 500):
-    for lr in (0.001,0.01,0.02,0.05,0.1, 0.2, 0.5, 0.75):
-        for decay in (1e-2*x for x in (0.01, 0.05, 0.1, 0.2, 0.5, 1., 2., 5., 10., 20., 50.)):
-            for mom in (0.1, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, .95):
-        
-                results.append((bs,lr,decay,mom,timed_sgd_OLS(x1,y1,x2,y2, lr = lr, decay=decay, momentum=mom, batch_size=bs)))
- 
-pprint.pprint(results)       
-
-
-# results = []
-#  
-# for lr in (0.001,0.01,0.02,0.05,0.1, 0.2, 0.5, 0.75):
-#     results.append((lr,timed_sgd_OLS(x1,y1,x2,y2, lr = lr)))
-#      
-# pprint.pprint(results)
-
-#best_lr = 0.01
-
-#results = []
-
-#for decay in (1e-2*x for x in (0.01, 0.05, 0.1, 0.2, 0.5, 1., 2., 5., 10., 20., 50.)):
-#    results.append((decay,timed_sgd_OLS(x1,y1,x2,y2, lr = best_lr, decay=decay)))
-
-#pprint.pprint(results)
-
-
-# def training_function( x, x1, y1, x2, y2 ):
-#     lr, decay, momentum, batch_size = x[0],x[1],x[2], 100
-#     tt, score = timed_sgd_OLS(x1=x1,y1=y1,x2=x2,y2=y2, lr=lr, decay=decay, nesterov=True, momentum=momentum, batch_size=batch_size )
-#     if isnan(score):
-#             score = 1000 
-#     return tt*tt+score*score
- 
- 
-#(0.1, 1e-2, 0.8, 100)
- 
- 
-# min_result = scipy_opt.minimize( training_function, (0.01, 5e-2, 0.8, 100), args=(x1,y1,x2,y2),
-#                                   method='L-BFGS-B', jac=None,
-#                                 bounds=((0.01,0.5),(1e-4,0.1),(0.1,0.95),(10, 200)) )
-
-
-# min_result = scipy_opt.basinhopping( training_function, (0.01, 5e-2, 0.8), minimizer_kwargs = { 'args' : (x1,y1,x2,y2) } )
-# 
-#   
-# pprint.pprint(min_result)
-
-#x': array([  1.00000030e-01,   1.00000068e-02,   8.00000011e-01,
-#         1.00000008e+02])}
-
-# results = timed_sgd_OLS(x1=x1,y1=y1,x2=x2,y2=y2, lr=1.00000030e-01, decay=1.00000068e-02, nesterov=True, momentum=8.00000011e-01, batch_size=100 )
-# 
-# pprint.pprint(results)
-
 
 #Problem 1: Write analogous functions
 def timed_sgd_Ridge(x1,y1,x2=None,y2=None,
                     lambda2=0.01, lr=0.1, decay=1e-2, nesterov=True, momentum=0.8, batch_size=100,nb_epoch=50):
     if (x2 is None)^(x2 is None): raise ValueError("if you specify x2 or y2 you need to specify the other as well")
     if x2 is None and y2 is None: x2=x1; y2=y1 #if no Cross-validation set, use original
+    print("Running SGD Ridge with lambda2: ", lambda2, " lr: ", lr, " decay: ", decay, " Momentum: ", momentum, " Batch size: ", batch_size)
+    np.random.seed(78)
     time0 = time() # Start timer
+    earlystopper = EarlyStopping(monitor='loss', patience=1)
     sgd=SGD(lr=lr, decay=decay, nesterov=nesterov, momentum=momentum)
     model = Sequential()
     model.add(Dense(1, input_dim=K, activation='linear', W_regularizer=l2(lambda2)))
     model.compile(loss='mse', optimizer=sgd)
-    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True)
+    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True, callbacks=[earlystopper], verbose=0)
     score = model.evaluate(x2, y2, batch_size=20)
     print("Score:",score,'\n',np.around(model.layers[0].get_weights()[0][:,0],2))
     print("Time:",time()-time0)
     return time()-time0, score
 
-
-
-
-#timed_sgd_Ridge(x1,y1,x2,y2)
-
 def timed_sgd_LASSO(x1,y1,x2=None,y2=None,
             lambda1=0.01, lr=0.1, decay=1e-2, nesterov=True, momentum=0.8, batch_size=100,nb_epoch=50):
     if (x2 is None)^(x2 is None): raise ValueError("if you specify x2 or y2 you need to specify the other as well")
     if x2 is None and y2 is None: x2=x1; y2=y1 #if no Cross-validation set, use original
+    print("Running SGD LASSO with lambda1: ", lambda1, " lr: ", lr, " decay: ", decay, " Momentum: ", momentum, " Batch size: ", batch_size)
+    np.random.seed(78)
     time0 = time() # Start timer
+    earlystopper = EarlyStopping(monitor='loss', patience=1)
     sgd=SGD(lr=lr, decay=decay, nesterov=nesterov, momentum=momentum)
     model = Sequential()
     model.add(Dense(1, input_dim=K, activation='linear', W_regularizer=l1(lambda1)))
     model.compile(loss='mse', optimizer=sgd)
-    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True)
+    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True,callbacks=[earlystopper], verbose=0)
     score = model.evaluate(x2, y2, batch_size=20)
     print("Score:",score,'\n',np.around(model.layers[0].get_weights()[0][:,0],2))
     print("Time:",time()-time0)
@@ -152,18 +95,264 @@ def timed_sgd_Elastic(x1,y1,x2=None,y2=None,
     lambda1=0.01, lambda2=0.01, lr=0.1, decay=1e-2, nesterov=True, momentum=0.8, batch_size=100,nb_epoch=50):
     if (x2 is None)^(x2 is None): raise ValueError("if you specify x2 or y2 you need to specify the other as well")
     if x2 is None and y2 is None: x2=x1; y2=y1 #if no Cross-validation set, use original
+    print("Running SGD Elastic with lambda1: ", lambda1, " lambda2: ", lambda2, " lr: ", lr, " decay: ", decay, " Momentum: ", momentum, " Batch size: ", batch_size)
+    np.random.seed(78)
     time0 = time() # Start timer
+    earlystopper = EarlyStopping(monitor='loss', patience=1)
     sgd=SGD(lr=lr, decay=decay, nesterov=nesterov, momentum=momentum)
     model = Sequential()
     model.add(Dense(1, input_dim=K, activation='linear', W_regularizer=l1l2(lambda1,lambda2)))
     model.compile(loss='mse', optimizer=sgd)
-    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True)
+    model.fit(x1, y1,nb_epoch=nb_epoch,batch_size=batch_size, show_accuracy=True, callbacks=[earlystopper], verbose=0)
     score = model.evaluate(x2, y2, batch_size=20)
     print("Score:",score,'\n',np.around(model.layers[0].get_weights()[0][:,0],2))
     print("Time:",time()-time0)
     return time()-time0, score
 
-#timed_sgd_Elastic(x1,y1,x2,y2)
+
+runOLS = True
+runRidge = True
+runLASSO = True
+runElastic = True
+
+
+if(runOLS):
+    
+    best_lr = 0.1
+    best_decay = 0.01
+    best_mom = 0.8
+    best_size = 100
+    best_lambda1 = 0.01
+    best_lambda2 = 0.01
+    best_score = 999999
+    best_tt = 999999
+
+    with open("OLS_out.txt", "w") as fout:
+        
+        for lr in np.arange(0.05,0.5, 0.05):
+            for decay in (0.01*x for x in np.arange(0.5, 10.5, 0.5)):
+                tt, score = timed_sgd_OLS(x1,y1,x2,y2, lr = lr, decay=decay, momentum=best_mom, batch_size=best_size)
+                if score < best_score:
+                    best_lr = lr
+                    best_decay = decay
+                    best_score = score
+                    best_tt = tt
+                print((best_size,lr,decay,best_mom,tt,score), file=fout)
+            fout.flush()    
+                
+                
+             
+        print( "Best Score: ", best_score, " Best lr: ", best_lr, "Best decay: ", best_decay)
+        
+        for mom in np.arange(0.01,0.95, 0.01):
+            tt, score = timed_sgd_OLS(x1,y1,x2,y2, lr = best_lr, decay=best_decay, momentum=mom, batch_size=best_size)
+            if score < best_score:
+                best_mom = mom
+                best_score = score
+                best_tt = tt
+                print((best_size,best_lr,best_decay,mom,tt,score), file=fout)
+            fout.flush()  
+        print( "Best Score: ", best_score, " Best Momentum: ", best_mom )
+        
+        for bs in range(50, 510, 10 ):
+            tt, score = timed_sgd_OLS(x1,y1,x2,y2, lr = best_lr, decay=best_decay, momentum=best_mom, batch_size=bs)
+            if score < best_score:
+                best_size = bs
+                best_score = score
+            print((bs,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()   
+        print( "Best Score: ", best_score, " Best size: ", best_size)
+    
+        print((best_lr, best_decay, best_mom, best_size, best_tt, best_score), file=fout)
+          
+
+if(runRidge):
+    
+    
+    best_lr = 0.1
+    best_decay = 0.01
+    best_mom = 0.8
+    best_size = 100
+    best_lambda1 = 0.01
+    best_lambda2 = 0.01
+    best_score = 999999
+    best_time = 999999
+    best_tt = 999999
+    
+    
+    with open("Ridge_out.txt", "w") as fout:
+        
+        for lambda2 in np.arange(0.005,0.055, 0.005):
+            tt, score = timed_sgd_Ridge(x1,y1,x2,y2, lr = best_lr, lambda2 = lambda2, decay=best_decay, momentum=best_mom, batch_size=best_size)
+            if score < best_score:
+                best_lambda2 = lambda2
+                best_score = score
+                best_tt = tt
+            print((lambda2,best_size,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()  
+        
+        for lr in np.arange(0.05,0.5, 0.05):
+            for decay in (0.01*x for x in np.arange(0.5, 10.5, 0.5)):
+                tt, score = timed_sgd_Ridge(x1,y1,x2,y2, lr = lr, lambda2 = best_lambda2, decay=decay, momentum=best_mom, batch_size=best_size)
+                if score < best_score:
+                    best_lr = lr
+                    best_decay = decay
+                    best_score = score
+                    best_tt = tt
+                print((best_lambda2,best_size,lr,decay,best_mom,tt,score), file=fout)
+            fout.flush()    
+                
+                
+             
+        print( "Best Score: ", best_score, " Best lr: ", best_lr, "Best decay: ", best_decay)
+        
+        for mom in np.arange(0.01,0.95, 0.01):
+            tt, score = timed_sgd_Ridge(x1,y1,x2,y2, lr = best_lr, lambda2 = best_lambda2, decay=best_decay, momentum=mom, batch_size=best_size)
+            if score < best_score:
+                best_mom = mom
+                best_score = score
+                best_tt = tt
+            print((best_lambda2,best_size,best_lr,best_decay,mom,tt,score), file=fout)
+            fout.flush()  
+        print( "Best Score: ", best_score, " Best Momentum: ", best_mom )
+        
+        
+        for bs in range(50, 510, 10 ):
+            tt, score = timed_sgd_Ridge(x1,y1,x2,y2, lr = best_lr, lambda2 = best_lambda2, decay=best_decay, momentum=best_mom, batch_size=bs)
+            if score < best_score:
+                best_size = bs
+                best_score = score
+                best_tt = tt
+            print((best_lambda2,bs,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()
+        print( "Best Score: ", best_score, " Best size: ", best_size)
+    
+        print((best_lambda2, best_lr, best_decay, best_mom, best_size, best_tt, best_score), file=fout)
+
+
+
+if(runLASSO):
+    
+    best_lr = 0.1
+    best_decay = 0.01
+    best_mom = 0.8
+    best_size = 100
+    best_lambda1 = 0.01
+    best_lambda2 = 0.01
+    best_score = 999999
+    best_tt = 999999
+    
+    
+    with open("LASSO_out.txt", "w") as fout:
+        
+        for lambda1 in np.arange(0.005,0.055, 0.005):
+            tt, score = timed_sgd_LASSO(x1,y1,x2,y2, lr = best_lr, lambda1 = lambda1, decay=best_decay, momentum=best_mom, batch_size=best_size)
+            if score < best_score:
+                best_lambda1 = lambda1
+                best_score = score
+                best_tt = tt
+            print((lambda1,best_size,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()  
+        
+        for lr in np.arange(0.05,0.5, 0.05):
+            for decay in (0.01*x for x in np.arange(0.5, 10.5, 0.5)):
+                tt, score = timed_sgd_LASSO(x1,y1,x2,y2, lr = lr, lambda1 = best_lambda1, decay=decay, momentum=best_mom, batch_size=best_size)
+                if score < best_score:
+                    best_lr = lr
+                    best_decay = decay
+                    best_score = score
+                print((best_lambda1,best_size,lr,decay,best_mom,tt,score), file=fout)
+            fout.flush()    
+                
+                
+             
+        print( "Best Score: ", best_score, " Best lr: ", best_lr, "Best decay: ", best_decay)
+        
+        for mom in np.arange(0.01,0.95, 0.01):
+            tt, score = timed_sgd_LASSO(x1,y1,x2,y2, lr = best_lr, lambda1 = best_lambda1, decay=best_decay, momentum=mom, batch_size=best_size)
+            if score < best_score:
+                best_mom = mom
+                best_score = score
+                best_tt = tt
+            print((best_lambda1,best_size,best_lr,best_decay,mom,tt,score), file=fout)
+            fout.flush()  
+        print( "Best Score: ", best_score, " Best Momentum: ", best_mom )
+        
+        
+        for bs in range(50, 510, 10 ):
+            tt, score = timed_sgd_LASSO(x1,y1,x2,y2, lr = best_lr, lambda1 = best_lambda1, decay=best_decay, momentum=best_mom, batch_size=bs)
+            if score < best_score:
+                best_size = bs
+                best_score = score
+            print((best_lambda1,bs,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()
+        print( "Best Score: ", best_score, " Best size: ", best_size)
+    
+        print((best_lambda1, best_lr, best_decay, best_mom, best_size, best_tt, best_score), file=fout)
+
+
+if(runElastic):
+    
+    best_lr = 0.1
+    best_decay = 0.01
+    best_mom = 0.8
+    best_size = 100
+    best_lambda1 = 0.01
+    best_lambda2 = 0.01
+    best_score = 999999
+    best_tt = 999999
+    
+    
+    with open("Elastic_out.txt", "w") as fout:
+        
+        for lambda1 in np.arange(0.005,0.055, 0.005):
+            for lambda2 in np.arange(0.005,0.055, 0.005):
+                tt, score = timed_sgd_Elastic(x1,y1,x2,y2, lr = best_lr, lambda1 = lambda1, lambda2 = lambda2, decay=best_decay, momentum=best_mom, batch_size=best_size)
+                if score < best_score:
+                    best_lambda2 = lambda2
+                    best_lambda1 = lambda1
+                    best_score = score
+                    best_tt = tt
+                print((lambda1, lambda2, best_size,best_lr,best_decay,best_mom,tt,score), file=fout)
+                fout.flush()    
+        
+        for lr in np.arange(0.05,0.5, 0.05):
+            for decay in (0.01*x for x in np.arange(0.5, 10.5, 0.5)):
+                tt, score = timed_sgd_Elastic(x1,y1,x2,y2, lr = lr, lambda1 = best_lambda1, lambda2 = best_lambda2, decay=decay, momentum=best_mom, batch_size=best_size)
+                if score < best_score:
+                    best_lr = lr
+                    best_decay = decay
+                    best_score = score
+                    best_tt = tt
+                print((best_lambda1,best_size,lr,decay,best_mom,tt,score), file=fout)
+            fout.flush()    
+                
+                
+             
+        print( "Best Score: ", best_score, " Best lr: ", best_lr, "Best decay: ", best_decay)
+        
+        for mom in np.arange(0.01,0.95, 0.01):
+            tt, score = timed_sgd_Elastic(x1,y1,x2,y2, lr = best_lr, lambda1 = best_lambda1, lambda2 = best_lambda2, decay=best_decay, momentum=mom, batch_size=best_size)
+            if score < best_score:
+                best_mom = mom
+                best_score = score
+                best_tt = tt
+            print((best_lambda1,best_lambda2,best_size,best_lr,best_decay,mom,tt,score), file=fout)
+            fout.flush()  
+        print( "Best Score: ", best_score, " Best Momentum: ", best_mom )
+           
+        
+        for bs in range(50, 510, 10 ):
+            tt, score = timed_sgd_Elastic(x1,y1,x2,y2, lr = best_lr, lambda1 = best_lambda1, lambda2 = best_lambda2, decay=best_decay, momentum=best_mom, batch_size=bs)
+            if score < best_score:
+                best_size = bs
+                best_score = score
+                best_tt = tt
+            print((best_lambda1,best_lambda2,bs,best_lr,best_decay,best_mom,tt,score), file=fout)
+            fout.flush()
+        print( "Best Score: ", best_score, " Best size: ", best_size)
+    
+        print((best_lambda1, best_lambda2, best_lr, best_decay, best_mom, best_size, best_tt, best_score), file=fout)
 
 #Problem 2: 
 #Part A: Your goal here is to obtain better time and score than default parameters for each of the four above functions:
